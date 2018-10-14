6a193e894761dfae936de3ef87d8cfbc358eeea1
FatemaKhan <43725550+FatemaKhan@users.noreply.github.com>
2018-10-14 06:29:15 -0900

    d

---
+++ facebook.java
@@ -0,0 +1,20 @@
+
+public class facebook {
+
+	public static void main(String[] args) {
+		//WebDriver driver= new ChromeDriver;
+		//driver.get(https://www.facebook.com/);
+		
+		System.out.println(System.getProperty("user.dir"));
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
